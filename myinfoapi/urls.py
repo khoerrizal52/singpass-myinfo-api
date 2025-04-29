542ff921b45ca9e54fc9f38950450a7bd856e5ea
Khoerurrizal <khoerrizal@gmail.com>
2025-04-29 08:46:22 +0700

    add api base authorize singpass

--- myinfoapi/urls.py
+++ myinfoapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, singpass_auth, singpass_auth_callback
 
 urlpatterns = [
     path('', home, name='home'),
+    path('singpass-auth', singpass_auth, name='singpass_auth'),
+    path('singpass-auth/callback', singpass_auth_callback, name='singpass_auth_callback'),
 ]
--- myinfoapi/views.py
+++ myinfoapi/views.py
@@ -1,6 +1,34 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from myinfo.client import MyInfoPersonalClientV4
+from django.utils.crypto import get_random_string
+from django.conf import settings
+from rest_framework import status
 
 @api_view(['GET'])
 def home(request):
     return Response("myinfo api is running")
+
+@api_view(['GET'])
+def singpass_auth(request):
+    oauth_state = get_random_string(length=16)
+    callback_url = settings.FRONTEND_CALLBACK_URL
+
+    client = MyInfoPersonalClientV4()
+    res = client.get_authorise_url(oauth_state, callback_url)
+
+    return Response({"url-redirect": res, "oauth_state": oauth_state})
+
+@api_view(['GET'])
+def singpass_auth_callback(request):
+    auth_code = request.query_params.get('code')
+    oauth_state = request.query_params.get('oauth_state')
+
+    if not auth_code:
+        return Response(
+            {"error": "Authorization code not provided."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    callback_url = settings.FRONTEND_CALLBACK_URL
+    person_data = MyInfoPersonalClientV4().retrieve_resource(auth_code, oauth_state, callback_url)
+    return Response({"person_data": person_data})
